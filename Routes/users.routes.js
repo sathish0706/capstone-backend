baba67301b69edea2a44a4b0132d6a717c79c716
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-03-26 14:13:31 +0100

    Update users.routes.js

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getUser } = require("../Controllers/users.controllers");
-const { isAuth } = require("../utils/auth");
-const { isAdmin } = require("../utils/athorised");
+// const { isAuth } = require("../utils/auth");
+// const { isAdmin } = require("../utils/athorised");
 
 const router = express.Router();
 
-router.get("/user", isAuth, isAdmin, getUser);
+router.get("/user", getUser);
 
 module.exports = router;
